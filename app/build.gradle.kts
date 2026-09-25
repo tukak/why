fcc9dc2321f16a56ba790bf82c14be909739e27b
tukak <lukas@kutner.cz>
2026-09-25 16:25:11 +0200

    Release 1.0.1

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -14,8 +14,8 @@ android {
         applicationId = "cz.kutner.why"
         minSdk = 31
         targetSdk = 37
-        versionCode = 10000
-        versionName = "1.0.0"
+        versionCode = 10001
+        versionName = "1.0.1"
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
     }
 
